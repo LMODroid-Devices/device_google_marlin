d7a382c195a87dfc6e8a2834fee83b30ce582fe5
LMO GerritBot <gerrit@libremobileos.com>
2023-09-08 04:08:36 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_sailfish.mk \
-	$(LOCAL_DIR)/lineage_marlin.mk \
-	$(LOCAL_DIR)/lineage_sailfish.mk
+	$(LOCAL_DIR)/lmodroid_marlin.mk \
+	$(LOCAL_DIR)/lmodroid_sailfish.mk
 
 COMMON_LUNCH_CHOICES := \
 	aosp_marlin-userdebug \
